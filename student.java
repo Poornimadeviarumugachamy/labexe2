cf70bb1eb93455e9d6b09234a1bd8db5bc9cd075
Poornimadeviarumugachamy <68942680+Poornimadeviarumugachamy@users.noreply.github.com>
2020-08-13 13:19:06 +0530

    Add files via upload
    team1

--- student.java
+++ student.java
@@ -4,11 +4,62 @@
  * and open the template in the editor.
  */
 package labexe2;
+import java.util.Scanner;
+        
 
 /**
  *
  * @author ARUMUGASAMY
  */
-public class student {
+
+class student{
+    float AP;
+    String name="";
+    int reg;
+    int total;
+    int s;
+    void attendance()
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the student name");
+        name=obj.nextLine();
+        System.out.println("Enter the registration number");
+        reg=obj.nextInt();
+        System.out.println("Enter the total number of working days");
+        total=obj.nextInt();
+        System.out.println("Enter the total number of days the student was present");
+        s=obj.nextInt();
+        AP=(s/total)*100;
+        System.out.println("Attendance percentage of this student is"+AP);
+        if(AP>=90)
+            System.out.println("5 marks");
+        else if(AP>=80 && AP<90)
+            System.out.println("4 marks");
+        else if(AP>=75 && AP<80)
+            System.out.println("3 marks");
+        else if (AP<75)
+            System.out.println("0 marks");
+    
     
 }
+}
+
+public class attendnance{
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number of students");
+        int n=obj.nextInt();
+        student obj3 [] = new student[100];
+        int a;
+        for(a=0;a<n;a++)
+        {
+            obj3[a] = new student();
+            obj3[a].student();
+        }   
+    }
+    
+}
+
